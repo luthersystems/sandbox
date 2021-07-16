ebfc40ad7352222a1d921ffea8739c660e2d940c
Sam Wood <sam.wood@luthersystems.com>
2021-07-16 09:52:32 -0700

    Fix tests-api make target

--- tests/cat-postman-collections-docker.sh
+++ tests/cat-postman-collections-docker.sh
@@ -5,5 +5,5 @@
 SCRIPT="${BASH_SOURCE:-$0}"
 SOURCE_DIR=$(dirname "$SCRIPT")
 
-MARTIN=$(cd ${SOURCE_DIR}/.. && make martincmd)
-${MARTIN} ${SOURCE_DIR}/cat-postman-collections.sh "$@"
+MARTIN=$(cd ${SOURCE_DIR} && make martincmd)
+${MARTIN} cat-postman-collections.sh "$@"
