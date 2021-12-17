c82a3c718f7664bc8cae6455a245ccd6b7e386b1
Jacob Kopczynski <jkopczyn@users.noreply.github.com>
2021-12-17 01:34:28 +0000

    Tutorial diffs #7-#10

--- phylum/utils.lisp
+++ phylum/utils.lisp
@@ -9,9 +9,15 @@
 ;; This block defines helper functions for account objects in the data model.
 ;; The helper functions have their own internal, private helpers which are
 ;; defined by labels.
-(labels ([make-account (acct-id balance)
+(labels ([make-account (acct-id balance &optional name)
           (sorted-map "account_id" acct-id
+                      "name" name
                       "current_balance" (to-int balance))]
+         [put-account-name! (acct-id name)
+          (statedb:put (format-string "{}.name" acct-id) name)
+          true]
+         [get-account-name (acct-id)
+          (statedb:get (format-string "{}.name" acct-id))]
          [get-balance (acct-id)
           (cc:infof (sorted-map "account_id" acct-id) "GET")
           (let* ([balance (statedb:get acct-id)])
@@ -24,20 +30,20 @@
         ;; create-account! creates a new account record in statedb and records an
         ;; account object.  If the given acct-id already exists in statedb
         ;; create-account! does nothing and returns nil.
-        (defun create-account! (acct-id balance)
+        (defun create-account! (acct-id balance &optional name)
           (account-do acct-id
                       (lambda (found? _)
                         (and (not found?)
                              (>= balance 0)
                              (put-account! acct-id balance)
-                             (make-account acct-id balance)))))
-
+                             (put-account-name! acct-id name)
+                             (make-account acct-id balance name)))))
         ;; get-account retrieves an account record from statedb.  If the given
         ;; account does not exist a nil value is returned.
         (defun get-account (acct-id)
           (account-do acct-id
                       (lambda (found? balance)
-                        (if found? (make-account acct-id balance) '()))))
+                        (if found? (make-account acct-id balance (get-account-name acct-id)) '()))))
 
         ;; set-balance! sets the balance in the specified account and returns true.
         ;; If the specified account does not exist set-balance! returns false.
