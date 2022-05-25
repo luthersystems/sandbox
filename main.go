700234f18d45616061d04a9a73d45225e6394a54
Jacob Kopczynski <jacob.kopczynski@luthersystems.com>
2022-05-25 12:39:02 -0700

    Extract send method

--- main.go
+++ main.go
@@ -31,75 +31,88 @@
 package main
 
 import (
-    "context"
-    "encoding/json"
-    "fmt"
-    "os"
-    "time"
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+	"time"
 
-    datadog "github.com/DataDog/datadog-api-client-go/api/v2/datadog"
+	datadog "github.com/DataDog/datadog-api-client-go/api/v2/datadog"
 )
 
+type datadogSetup struct {
+	Config *datadog.Configuration
+	Client *datadog.APIClient
+}
+
+var defaultDatadogSetup datadogSetup
+
+func sendBody(ctx context.Context, body datadog.MetricPayload) error {
+	payload, resp, err := defaultDatadogSetup.Client.MetricsApi.SubmitMetrics(ctx, body, *datadog.NewSubmitMetricsOptionalParameters())
+
+	fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", resp)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error when calling `MetricsApi.SubmitMetrics`: %v\n", err)
+		fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", resp)
+		return err
+	}
+
+	payloadContent, _ := json.MarshalIndent(payload, "", "  ")
+	fmt.Fprintf(os.Stdout, "Intake Payload from `MetricsApi.SubmitMetrics`: %s\n", payloadContent)
+	return nil
+}
+
 func main() {
-    ctx := datadog.NewDefaultContext(context.Background())
-    configuration := datadog.NewConfiguration()
-    apiClient := datadog.NewAPIClient(configuration)
+	ctx := datadog.NewDefaultContext(context.Background())
+	defaultDatadogSetup.Config = datadog.NewConfiguration()
+	defaultDatadogSetup.Client = datadog.NewAPIClient(defaultDatadogSetup.Config)
 
-    bodies := [3]datadog.MetricPayload{
-        datadog.MetricPayload{
-            Series: []datadog.MetricSeries{
-                {
-                    Metric: "fnord.count",
-                    Type:   datadog.METRICINTAKETYPE_COUNT.Ptr(),
-                    Points: []datadog.MetricPoint{
-                        {
-                            Timestamp: datadog.PtrInt64(time.Now().Unix()),
-                            Value:     datadog.PtrFloat64(3.0),
-                        },
-                    },
-                },
-            },
-        },
-        datadog.MetricPayload{
-            Series: []datadog.MetricSeries{
-                {
-                    Metric: "fnord.count",
-                    Type:   datadog.METRICINTAKETYPE_COUNT.Ptr(),
-                    Points: []datadog.MetricPoint{
-                        {
-                            Timestamp: datadog.PtrInt64(time.Now().Unix()+40),
-                            Value:     datadog.PtrFloat64(2.0),
-                        },
-                    },
-                },
-            },
-        },
-        datadog.MetricPayload{
-            Series: []datadog.MetricSeries{
-                {
-                    Metric: "fnord.count",
-                    Type:   datadog.METRICINTAKETYPE_COUNT.Ptr(),
-                    Points: []datadog.MetricPoint{
-                        {
-                            Timestamp: datadog.PtrInt64(time.Now().Unix()+90),
-                            Value:     datadog.PtrFloat64(18.0),
-                        },
-                    },
-                },
-            },
-        },
-    }
+	bodies := [3]datadog.MetricPayload{
+		datadog.MetricPayload{
+			Series: []datadog.MetricSeries{
+				{
+					Metric: "fnord.count",
+					Type:   datadog.METRICINTAKETYPE_COUNT.Ptr(),
+					Points: []datadog.MetricPoint{
+						{
+							Timestamp: datadog.PtrInt64(time.Now().Unix()),
+							Value:     datadog.PtrFloat64(3.0),
+						},
+					},
+				},
+			},
+		},
+		datadog.MetricPayload{
+			Series: []datadog.MetricSeries{
+				{
+					Metric: "fnord.count",
+					Type:   datadog.METRICINTAKETYPE_COUNT.Ptr(),
+					Points: []datadog.MetricPoint{
+						{
+							Timestamp: datadog.PtrInt64(time.Now().Unix() + 40),
+							Value:     datadog.PtrFloat64(2.0),
+						},
+					},
+				},
+			},
+		},
+		datadog.MetricPayload{
+			Series: []datadog.MetricSeries{
+				{
+					Metric: "fnord.count",
+					Type:   datadog.METRICINTAKETYPE_COUNT.Ptr(),
+					Points: []datadog.MetricPoint{
+						{
+							Timestamp: datadog.PtrInt64(time.Now().Unix() + 90),
+							Value:     datadog.PtrFloat64(18.0),
+						},
+					},
+				},
+			},
+		},
+	}
 
-    for _, body := range bodies {
-        resp, r, err := apiClient.MetricsApi.SubmitMetrics(ctx, body, *datadog.NewSubmitMetricsOptionalParameters())
-
-        if err != nil {
-            fmt.Fprintf(os.Stderr, "Error when calling `MetricsApi.SubmitMetrics`: %v\n", err)
-            fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
-        }
-
-        responseContent, _ := json.MarshalIndent(resp, "", "  ")
-        fmt.Fprintf(os.Stdout, "Response from `MetricsApi.SubmitMetrics`:\n%s\n", responseContent)
-        fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
-    }
+	for _, body := range bodies {
+		sendBody(ctx, body)
+	}
 }
